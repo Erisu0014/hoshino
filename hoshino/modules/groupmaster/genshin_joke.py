55cc718e136083cde6367a27db47be1cda8ec778
nahida <1misserisu@gmail.com>
2022-11-10 10:32:20 +0800

    🐛修复原神笑话触发bug

--- hoshino/modules/groupmaster/genshin_joke.py
+++ hoshino/modules/groupmaster/genshin_joke.py
@@ -8,13 +8,13 @@
 
 import random
 
-from genshin_joke_text import joke
+from .genshin_joke_text import joke
 from hoshino import Service
 
 sv = Service('genshin_joke', visible=False)
 
 
 # basic function for debug, not included in Service('chat')
-@sv.on_keyword('我去 原', 'opssw', 'op', '原')
-async def genshin_joke(session):
-    await session.send(random.choice(joke))
+@sv.on_keyword('我去 原',  'op','叩')
+async def genshin_joke(bot,ev):
+    await bot.send(ev, random.choice(joke))
