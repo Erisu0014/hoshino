cb24e5a3463ca0bd4a70a0cdef06f888253c2e4c
nahida <1269732086@qq.com>
2023-02-02 09:55:59 +0800

    📝更新原神笑话

--- hoshino/modules/groupmaster/genshin_joke.py
+++ hoshino/modules/groupmaster/genshin_joke.py
@@ -9,13 +9,18 @@
 import random
 
 from .genshin_joke_text import joke
-from hoshino import Service
+from hoshino import Service, R
 from nonebot import CommandSession
 
 sv = Service('genshin_joke', visible=False)
 
 
 # basic function for debug, not included in Service('chat')
-@sv.on_command('我去 原',   aliases=('op','叩','我去 o','我去 原！'))
+@sv.on_command('我去 原', aliases=('op', '叩', '我去 o', '我去 原！'))
 async def genshin_joke(ss: CommandSession):
-    await ss.finish(random.choice(joke))
+    result = random.choice(joke)
+    if result[:3] == "pic":
+        pic = R.img(f'op/{result[3:]}').cqcode
+        await ss.send(pic)
+    else:
+        await ss.finish(result)
